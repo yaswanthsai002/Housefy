9a71010591f198b677dc7db29e2feda9b7fd333c
Kotagiri Yaswanth Sai <yaswanthsaikotagiri1@gmail.com>
2024-08-29 21:55:07 +0530

    added jwt mechanism

--- api/controllers/authentication.controller.mjs
+++ api/controllers/authentication.controller.mjs
@@ -1,47 +1,52 @@
 import bcrypt from "bcrypt";
 import { config } from "dotenv";
 import User from "../models/user.model.mjs";
+import jwt from "jsonwebtoken";
 
 config();
 
 const secretKey = process.env.SECRET_KEY;
-const saltRounds = process.env.NO_OF_SALT_ROUNDS;
+const saltRounds = parseInt(process.env.NO_OF_SALT_ROUNDS);
 
-// @desc SignIn
-// @route POST /api/signin
-// @access public
 export const signinAPI = async (req, res, next) => {
   try {
     const { email, password } = req.body;
-    const user = await User.find({ email: email });
-    if (!user || user.length === 0) {
-      return next({ statusCode: 404, message: "No user found" });
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return next({ statusCode: 404, message: "Invalid email or password" });
     }
+
     const combinedPassword = password + secretKey;
-    const userPassword = user[0].password;
     const passwordMatched = await bcrypt.compare(
       combinedPassword,
-      userPassword
+      user.password
     );
+
     if (!passwordMatched) {
-      return next({ statusCode: 401, message: "Invalid Credentials" });
+      return next({ statusCode: 401, message: "Invalid email or password" });
     }
-    res.status(200).json({ message: "Welcome to Housefy" });
+
+    const jwtToken = jwt.sign({ id: user._id }, secretKey, {
+      expiresIn: "1day",
+      algorithm: "HS512",
+    });
+    res
+      .cookie("jwt_token", jwtToken, { httpOnly: true })
+      .status(200)
+      .json({ message: "Welcome to Housefy" });
   } catch (err) {
     console.error("Error in signing in", err);
     return next({ statusCode: 500, message: "Error occurred in signing in" });
   }
 };
 
-// @desc SignUp
-// @route POST /api/signup
-// @access public
 export const signupAPI = async (req, res, next) => {
   try {
     const { firstName, lastName, email, password } = req.body;
+    const existingUser = await User.findOne({ email });
 
-    const existingUser = await User.find({ email: email });
-    if (existingUser && existingUser.length > 0) {
+    if (existingUser) {
       return next({
         statusCode: 400,
         message: "User already exists with this email",
@@ -49,10 +54,7 @@ export const signupAPI = async (req, res, next) => {
     }
 
     const combinedPassword = password + secretKey;
-    const hashedPassword = await bcrypt.hash(
-      combinedPassword,
-      parseInt(saltRounds)
-    );
+    const hashedPassword = await bcrypt.hash(combinedPassword, saltRounds);
 
     const newUser = new User({
       firstName,
